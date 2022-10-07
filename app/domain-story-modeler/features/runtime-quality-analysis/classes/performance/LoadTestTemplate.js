ede3b5916c8ef9d94eb23efd953b51ca3f6d091c
Dominik Kesim <Dominik.Kesim@gmail.com>
2022-10-07 18:23:41 +0200

    Feature: Adjusted loadtest template class

--- app/domain-story-modeler/features/runtime-quality-analysis/classes/performance/LoadTestTemplate.js
+++ app/domain-story-modeler/features/runtime-quality-analysis/classes/performance/LoadTestTemplate.js
@@ -4,16 +4,14 @@ export class LoadTestTemplate {
      * 
      * @param {*} loadTestDescription 
      * @param {*} serviceName 
-     * @param {*} selectedHttpEndpoints 
      * @param {*} loadTestDuration 
      * @param {*} numberOfSimulatedRequests 
      */
-    constructor(loadTestDescription, rampUpTime, serviceName, loadTestDuration, numberOfSimulatedRequests) {
+    constructor(loadTestDescription, serviceName, loadTestDuration, numberOfSimulatedRequests) {
         if (loadTestDescription == '') {
             this.serviceName = 'Very basic loadtest';
         }
         this.serviceName = serviceName;
-        this.rampUpTime = rampUpTime;
         this.loadTestDescription = loadTestDescription;
         this.loadTestDuration = loadTestDuration;
         this.numberOfSimulatedRequests = numberOfSimulatedRequests;
