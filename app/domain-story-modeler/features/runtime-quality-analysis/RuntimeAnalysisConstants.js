080324b0928ee8cbf10c7bf6ff8ebd24687c59ad
Dominik Kesim <Dominik.Kesim@gmail.com>
2022-11-16 11:19:53 +0100

    Refactor: removed unused constants from RuntimeAnalysisConstants

--- app/domain-story-modeler/features/runtime-quality-analysis/RuntimeAnalysisConstants.js
+++ app/domain-story-modeler/features/runtime-quality-analysis/RuntimeAnalysisConstants.js
@@ -3,16 +3,6 @@ export const LOADTEST_NAME = 'Loadtest';
 export const MONITORING_NAME = 'Monitoring';
 export const SERVICE_DELAY_NAME = 'ServiceDelay';
 
-export const SERVICE_FAILURE_NAME_INFO = 'Bitte geben Sie einen gültigen Service an!';
-export const RESILIENCE_SCENARIO_NAME_INFO = 'Bitte geben Sie einen gültigen Namen an!';
-export const RESILIENCE_SCENARIO_EXECUTION_ENVIRONMENT_INFO = 'Bitte geben Sie eine gültige Umgebung für das Szenario an!';
-export const SERVICE_FAILURE_AMOUNT_INFO = 'Bitte geben Sie eine gültige Anzahl an!';
-export const SERVICE_TIME_TO_FAILURE_INFO = 'Bitte geben Sie einen gültigen Zeitpunkt an!';
-export const RESILIENCE_FAULT_TYPE_INFO = 'Bitte wähle Sie die Art des Fehlers aus!';
-
-export const LOADTEST_DURATION_INFO = 'Please provide a valid number for the duration!';
-export const LOADTEST_NUMBER_OF_SIMULATED_REQUESTS_INFO = 'Please provide a valid positive number!';
-
 export const VERIFICATION_MODAL_NOTIFICATION = 'Wollen Sie wirklich dieses Template als Test erzeugen?';
 
 /**
